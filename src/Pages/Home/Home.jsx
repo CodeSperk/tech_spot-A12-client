95f6c271e1db69c9584b0702a9c568fae940011e
Mahbubur Rahman <inbx.mahbub@gmail.com>
2024-06-01 01:24:15 +0600

    added font and basic color setup and button styled at index.css

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,28 @@
 const Home = () => {
   return (
-    <div>
-      Welcome to TechSpot
+    <div >
+      <h1>Welcome to TechSpot</h1>
+      <h1>Welcome to TechSpot</h1>
+      <h2>Welcome to TechSpot</h2>
+      <h2>Welcome to TechSpot</h2>
+      <h3>Welcome to TechSpot</h3>
+      <h3>Welcome to TechSpot</h3>
+      <h4>Welcome to TechSpot</h4>
+      <h4>Welcome to TechSpot</h4>
+      <h5>Welcome to TechSpot</h5>
+      <h5>Welcome to TechSpot</h5>
+      <h6>Welcome to TechSpot</h6>
+      <h6>Welcome to TechSpot</h6>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio placeat officiis ullam nesciunt voluptatem quod quia iusto accusantium obcaecati maiores. Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, minus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam aperiam ex quasi eveniet laborum nemo tenetur fugiat molestias suscipit quisquam.
+
+      </p>
+
+      <button className="btn">Subscribe</button>
+  
+
+
+     
     </div>
   );
 };
