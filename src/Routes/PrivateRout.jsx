284650e5787cc08c343f59f823bb3df920af61b0
Mahbubur Rahman <inbx.mahbub@gmail.com>
2024-06-02 01:40:05 +0600

    added loading spinner

--- src/Routes/PrivateRout.jsx
+++ src/Routes/PrivateRout.jsx
@@ -1,13 +1,14 @@
 import { Navigate, useLocation } from "react-router-dom";
 import useAuth from "../Hooks/useAuth";
 import PropTypes from 'prop-types';
+import { Spinner } from "@material-tailwind/react";
 
 const PrivateRout = ({children}) => {
   const {user, loading} = useAuth();
   const location = useLocation();
 
   if(loading){
-    return <h2>Loading....</h2>
+    return <div className="h-screen w-full flex justify-center items-center"> <Spinner color="red" className="h-16 w-16" /> </div>
   }
   if(user){
     return children;
