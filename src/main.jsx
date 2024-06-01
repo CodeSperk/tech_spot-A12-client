966c7360a180998591e5d027ac5b98ba5847b057
Mahbubur Rahman <inbx.mahbub@gmail.com>
2024-06-02 02:27:47 +0600

    checked admin status using tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,18 @@ import { RouterProvider } from "react-router-dom";
 import MainRoutes from "./Routes/MainRoutes";
 import { ThemeProvider } from "@material-tailwind/react";
 import AuthProvider from "./Provider/AuthProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <ThemeProvider>
-        <RouterProvider router={MainRoutes} />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider>
+          <RouterProvider router={MainRoutes} />
+        </ThemeProvider>
+      </QueryClientProvider>
     </AuthProvider>
   </React.StrictMode>
 );
