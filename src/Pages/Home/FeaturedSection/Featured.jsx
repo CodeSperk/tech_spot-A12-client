267e72453c24f36c635d80a3d772719d03901766
Mahbubur Rahman <inbx.mahbub@gmail.com>
2024-06-02 22:23:18 +0600

    displayed featured products

--- src/Pages/Home/FeaturedSection/Featured.jsx
+++ src/Pages/Home/FeaturedSection/Featured.jsx
@@ -1,19 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../../../Hooks/useAxiosPublic";
+import { BiSolidUpvote } from "react-icons/bi";
 
 const Featured = () => {
   const axiosPublic = useAxiosPublic();
 
-  const {isPending, data: products = [] } = useQuery({
-    queryKey:["fProducts"],
+  const { isPending, data: products = [] } = useQuery({
+    queryKey: ["fProducts"],
     queryFn: async () => {
       const res = await axiosPublic("/featured");
       return res.data;
-    }
-  })
+    },
+  });
 
   {
-    isPending && <div> Loading ...</div>
+    isPending && <div> Loading ...</div>;
   }
   console.log(products);
 
@@ -24,12 +25,44 @@ const Featured = () => {
         <h2>Featured Products</h2>
         <div className="bg-[var(--clr-focussed)] h-1 w-28 mx-auto mt-4"></div>
       </header>
-      
+
       {/* Products */}
-      <div>
+      <div className="mt-8 md:mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8">
+        {products.map((product) => (
+          <div key={product._id} className="border py-6 rounded-xl shadow-md hover:shadow-sm px-4 lg:px-6">
 
+            {/* Name and vote */}
+            <div className="flex items-center justify-between">
+              <h5 className="hover:text-[var(--clr-focussed)] hover:underline cursor-pointer">
+                {product.productName}
+              </h5>
+              <button className="border border-[var(--clr-focussed)] text-[var(--clr-focussed)] rounded-md text-sm px-2 flex items-center gap-2 font-semibold" title="Vote Now">
+                {product?.upvote} <BiSolidUpvote />
+              </button>
+            </div>
+
+            {/* Product Image */}
+            <div className="bg-[var(--bg-primary)] rounded-xl p-6 my-6">
+              <div className="h-40 mx-auto">
+                <img
+                  src={product.productImage}
+                  alt=""
+                  className="rounded-xl h-full w-auto mx-auto"
+                />
+              </div>
+            </div>
+
+            {/* tags */}
+            <div className="flex gap-4 flex-wrap">
+              {product?.tags.map((tag, idx) => (
+                <small key={idx} className="text-[var(--clr-secondary)] font-medium text-sm">
+                  {tag}
+                </small>
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
-
     </div>
   );
 };
